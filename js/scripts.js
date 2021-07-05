c1e755ed598714f4218e5ee69a9ec7cc1c7ab70c
Jacob Strong <rjts2896@gmail.com>
2021-07-05 15:06:41 -0700

    refactor business logic to fix bug where higher numbers didn't supercede lower numbers

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@
 function rodgIfy(inputtedNumber) {
   let numbersArray = [];
   for (let i=0; i <= inputtedNumber; i++) {
-    if (i.toString().includes("1")) {
-      numbersArray.push("Beep!");
+    if (i.toString().includes("3")) {
+      numbersArray.push("Won't you be my neighbor?");
     }
     else if (i.toString().includes("2")) {
       numbersArray.push("Boop!");
     }
-    else if (i.toString().includes("3")) {
-      numbersArray.push("Won't you be my neighbor?");
+    else if (i.toString().includes("1")) {
+      numbersArray.push("Beep!");
     }
     else {
       numbersArray.push(i.toString());
