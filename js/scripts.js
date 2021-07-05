94bf7b65172514537af5ac0937374019c1c63fba
Jacob Strong <rjts2896@gmail.com>
2021-07-04 22:17:02 -0700

    add function to convert numbers containing 1 to Beep! while showing all numbers leading up to the user-inputted number

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@
 function rodgIfy(inputtedNumber) {
   let numbersArray = [];
   for (let i=0; i <= inputtedNumber; i++) {
+    if (i.toString().includes("1")) {
+      numbersArray.push("Beep!");
+    }
+    else {
+      numbersArray.push("" + i);
+    }
+  }
+  return numbersArray;
+};
